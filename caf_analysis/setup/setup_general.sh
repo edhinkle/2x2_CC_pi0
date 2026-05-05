b0cc90976a98ca05d238499e39c0409fbc40eb29
Elise Dianne Hinkle <ehinkle@login26.chn.perlmutter.nersc.gov>
2026-05-05 06:22:38 -0700

    Updating setup scripts.

--- caf_analysis/setup/setup_general.sh
+++ caf_analysis/setup/setup_general.sh
@@ -1,37 +1,14 @@
-#!/usr/bin/env bash
-
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup edepsim v3_2_0c -q e20:prof
 setup duneanaobj v03_06_01b -q e20:prof
 setup dk2nugenie   v01_10_01k -q debug:e20
 setup cmake v3_27_4
 setup geant4 v4_11_2_p02 -q "e26:prof"
-
-# Below is copied from NDCAFMaker setup script
-setup gcc v9_3_0
-setup pycurl
-setup ifdhc
-setup genie_xsec   v3_04_00 -q AR2320i00000:e1000:k250
-setup genie_phyopt v3_04_00 -q dkcharmtau
-setup jobsub_client
-setup eigen v3_3_5
-setup srproxy v00.44 -q py3913 #Added 
-setup hdf5 v1_10_5a -q e20
-setup fhiclcpp v4_15_03 -q debug:e20
-
-# edep-sim needs to know where a certain GEANT .cmake file is...
-G4_cmake_file=`find ${GEANT4_FQ_DIR}/lib64 -name 'Geant4Config.cmake'`
-export Geant4_DIR=`dirname $G4_cmake_file`
 
 # edep-sim needs to have the GEANT bin directory in the path
 export PATH=$PATH:$GEANT4_FQ_DIR/bin
 
 # shut up ROOT include errors
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$GENIE_INC/GENIE
-
-# add SRProxy path to ROOT to allow for use in ROOT macros
-# can use duneanaobj SRProxy
-#export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$SRPROXY_INC
 
 # nusystematics paths
 export NUSYST=/global/u2/r/rdiurba/nusystematics/nusystematics
@@ -50,8 +27,6 @@ mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export LD_LIBRARY_PATH=$mydir/lib:$LD_LIBRARY_PATH
 export PATH=$mydir/bin:$PATH
 
-# our FCL needs to be findable too -- we don't have a FHICL
-#export FHICL_FILE_PATH="$FHICL_FILE_PATH:$mydir/cfg"
 
 export ROOUNFOLD=/global/homes/r/rdiurba/cafAna/RooUnfold
 export LD_LIBRARY_PATH=$ROOUNFOLD:$LD_LIBRARY_PATH
@@ -86,8 +61,6 @@ export hep_concurrency_ROOT=${mywd}/fhicl-cpp-standalone/build/hep-concurrency-i
 export PATH=${hep_concurrency_ROOT}/bin/:${PATH}
 export LD_LIBRARY_PATH=${hep_concurrency_ROOT}/lib/:${LD_LIBRARY_PATH}
 
+
 source ${NUSYST}/build/Linux/bin/setup.nusystematics.sh
 export PATH=/global/u2/r/rdiurba/nusystematics/ndnusyst/ndnusyst-src/build/Linux/bin/:${PATH}
-
-source ../../2x2_sim/util/init.inc.sh
-
