fc64f3caf51cb2fed69c8c6fffc4858f2deb9f11
Elise Dianne Hinkle <ehinkle@login22.chn.perlmutter.nersc.gov>
2026-05-28 15:41:11 -0700

    Adding stipulation to move output file to outputs directory after running analysis.

--- caf_analysis/spine_ana/runCC1pi0SelSPINE.sh
+++ caf_analysis/spine_ana/runCC1pi0SelSPINE.sh
@@ -7,6 +7,7 @@ INPUTFILELIST="$(pwd)/include/config_data/MiniRun65FileList.txt"
 OUTPUTFILE="testCC1pi0SelSPINE.root"
 MCONLYSTRING="1"
 CONFIGFILEPATH="$(pwd)/include/config_data/CC1pi0.yaml"
+OUTPUTDIR="$(pwd)/outputs/"
 
 # Compile analysis code
 echo "Compiling CC1pi0 selection code..."
@@ -21,6 +22,7 @@ source compileCC1pi0SelSPINE.sh
 #./fakedata_dlp_sel ${INPUTFILELIST} testFakeDataHighEReweight.root 1
 #./fakedata_dlp_selLowE ${INPUTFILELIST} testFakeDataLowEReweight.root 1
 
+mv ${OUTPUTFILE} ${OUTPUTDIR}
 #root -l -b -q unfoldGENIERW.C
 #root -l -b -q unfoldFluxRW.C 
 #root -l -b -q testEff.C
