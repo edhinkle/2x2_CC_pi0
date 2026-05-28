ddeded9160eea40a1e85f55fd2149c94038d4189
Elise Dianne Hinkle <ehinkle@login22.chn.perlmutter.nersc.gov>
2026-05-28 15:15:02 -0700

    Preparing for compilation and running analysis.

---
+++ caf_analysis/spine_ana/compileCC1pi0SelSPINE.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Compile the C++ code using g++. Partially based on https://github.com/rdiurba/2x2_trackMultStudies/blob/master/spineAna/compile.sh
+
+# Compile code
+g++ -std=c++17 -O2 -I. `root-config --cflags` \
+  spine_CC1pi0_sel.cc \
+  src/config/ConfigLoader.cc \
+  src/io/CAFUtils.cc \
+  src/plotting/HistogramManager.cc \
+  src/plotting/CutFlowManager.cc \
+  src/plotting/RecoHists.cc \
+  src/plotting/TruthHists.cc \
+  src/plotting/TruthMatchedHists.cc \
+  src/selection/RecoSelection.cc \
+  src/selection/TruthSelection.cc \
+  `root-config --libs` \
+  -o spine_CC1pi0_sel
+
+#g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` dlp_sel.cc ../fluxSyst/FluxCovarianceReweight.cc -o dlp_sel -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_06_01b/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord_dict
+
+#g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` dlp_genieRw_sel.cc ../fluxSyst/FluxCovarianceReweight.cc  -o dlp_genieRw_sel -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_06_01b/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord_dict
+
+#g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` dlp_fluxRw_sel.cc  ../fluxSyst/FluxCovarianceReweight.cc -o dlp_fluxRw_sel -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_06_01b/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord -L $DUNEANAOBJ_LIB -lduneanaobj_StandardRecord_dict
+
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+fi
---
+++ caf_analysis/spine_ana/runCC1pi0SelSPINE.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Run CC1pi0 analysis. 
+# Partially based on https://github.com/rdiurba/2x2_trackMultStudies/blob/master/spineAna/runAnalysis.sh
+
+# Set parameters for running analysis
+INPUTFILELIST="$(pwd)/include/config_data/MiniRun65FileList.txt"
+OUTPUTFILE="testCC1pi0SelSPINE.root"
+MCONLYSTRING="1"
+CONFIGFILEPATH="$(pwd)/include/config_data/CC1pi0.yaml"
+
+# Compile analysis code
+echo "Compiling CC1pi0 selection code..."
+source compileCC1pi0SelSPINE.sh
+
+# Run the analysis
+./spine_CC1pi0_sel ${INPUTFILELIST} ${OUTPUTFILE} ${MCONLYSTRING} ${CONFIGFILEPATH}
+#./dlp_fluxRw_sel ${INPUTFILELIST} testFluxRWSPINE.root 1
+#./dlp_genieRw_sel ${INPUTFILELIST} testGENIERWSPINE.root 1
+
+#./spine_CC1pi0_sel ${INPUTFILELIST} ${OUTPUTFILE} ${MCONLYSTRING} ${CONFIGFILEPATH}
+#./fakedata_dlp_sel ${INPUTFILELIST} testFakeDataHighEReweight.root 1
+#./fakedata_dlp_selLowE ${INPUTFILELIST} testFakeDataLowEReweight.root 1
+
+#root -l -b -q unfoldGENIERW.C
+#root -l -b -q unfoldFluxRW.C 
+#root -l -b -q testEff.C
+#root -l -b -q plotSPINE.C
